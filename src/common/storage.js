529983edd42125d46e61b1213fd4a74d78137fbc
Xiangjin Zhan <674039707@qq.com>
2022-03-15 20:29:26 +0800

    优化代码

---
+++ src/common/storage.js
@@ -0,0 +1,31 @@
+export function setStorage(key, value, timeout = 0) {
+  let now = new Date().valueOf();
+  const valueWithInfo = {
+    value,
+    storedAt: now,
+    expiredAt: timeout > 0 ? now + timeout : -1,
+  }
+  try{
+    wx.setStorageSync(key, valueWithInfo);
+    return true;
+  }catch(e){
+    console.error(e);
+    return false;
+  }
+}
+
+export function getStorage(key){
+  let valueWithInfo = wx.getStorageSync(key);
+  if(!valueWithInfo){
+    return null;
+  }
+
+  if(valueWithInfo.expiredAt < 0 || valueWithInfo.expiredAt > new Date().valueOf()){
+    return valueWithInfo.value;
+  }else{
+    wx.removeStorageSync(key)
+    return null;
+  }
+}
+
+
