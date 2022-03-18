a0769322ddfe045b475c8088c14acfad0dd1c622
Xiangjin Zhan <674039707@qq.com>
2022-03-18 16:11:01 +0800

    添加全局函数、常数

--- src/common/param-conversion.js
+++ src/common/param-conversion.js
@@ -1,6 +1,36 @@
-import {autoCorrectRange} from "./constant";
+import {subjectRange, typeRange} from "./constant";
+import {isValidValue} from "@/common/utils";
 
-export const convertExercise = (exercise) => {
-    const {EautoCorrect} = exercise;
-    exercise['EautoCorrect'] = autoCorrectRange[EautoCorrect].value;
+export const getSubjectAndType = (value) => {
+    if(!Array.isArray(value)){
+        return {error: "value is not an array"};
+    }
+    const [subjectIndex, typeIndex] = value;
+    if(!isValidValue(subjectIndex) || !isValidValue(typeIndex)){
+        return {error: "invalid index"};
+    }
+    const subject = subjectRange[subjectIndex];
+    const type = typeRange[subject][typeIndex];
+    return {subject, type};
+}
+
+export const getSubjectAndTypeIndex = (subject, type) => {
+    const index = [];
+    for(let i = 0; i < subjectRange.length; i++){
+        if(subjectRange[i] === subject){
+            index.push(i);
+        }
+    }
+    if(index.length < 1){
+        return [null, null];
+    }
+    for(let i = 0; i < typeRange[subject].length; i++){
+        if(typeRange[subject][i] === type){
+            index.push(i);
+        }
+    }
+    if(index.length < 2){
+        index.push(0);
+    }
+    return index;
 }
